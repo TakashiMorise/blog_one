593a781bc2a831650c0ae3a3dde9abe989bbd0da
森瀬 隆司 <55taac.tac@gmail.com>
2018-12-01 17:17:26 +0900

    Add new and create action

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,4 +4,17 @@ class BlogsController < ApplicationController
     @blogs = Blog.all
   end
 
+  def new
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to controller: :blogs, action: :index
+  end
+
+  private
+  def blog_params
+    params.permit(:text)
+  end
+
 end
