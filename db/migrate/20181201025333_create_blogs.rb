6972f261c820d94e0bbc8436c671b26725d438d4
森瀬 隆司 <55taac.tac@gmail.com>
2018-12-01 14:00:27 +0900

    Create blogs-table

--- db/migrate/20181201025333_create_blogs.rb
+++ db/migrate/20181201025333_create_blogs.rb
@@ -2,7 +2,6 @@ class CreateBlogs < ActiveRecord::Migration[5.0]
   def change
     create_table :blogs do |t|
       t.text :text
-      t.references :user_id, null: false, foreign_key: true
       t.timestamps
     end
   end
